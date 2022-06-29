91f76392935223a3284f0060dd52ca0fc92fa445
Kristina Krylova <kriskamumu@gmail.com>
2022-06-30 00:32:18 +0300

    added User and UserCredentials

---
+++ src/main/java/ru/yandex/burgers/model/User.java
@@ -0,0 +1,36 @@
+package ru.yandex.burgers.model;
+
+public class User {
+
+    private final String email;
+    private final String password;
+    private final String name;
+
+    public User(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
---
+++ src/main/java/ru/yandex/burgers/model/UserCredentials.java
@@ -0,0 +1,29 @@
+package ru.yandex.burgers.model;
+
+public class UserCredentials {
+
+    private final String email;
+    private final String password;
+
+    public UserCredentials(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserCredentials{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
+
