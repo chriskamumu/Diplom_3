a0b326520d997c8daef4a936b693d1abdcf918a2
Kristina Krylova <kriskamumu@gmail.com>
2022-07-05 21:47:26 +0300

    added lombok

--- src/main/java/ru/yandex/burgers/model/User.java
+++ src/main/java/ru/yandex/burgers/model/User.java
@@ -1,36 +1,13 @@
 package ru.yandex.burgers.model;
 
+import lombok.Data;
+
+@Data
 public class User {
 
     private final String email;
     private final String password;
     private final String name;
 
-    public User(String email, String password, String name) {
-        this.email = email;
-        this.password = password;
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
 
--- src/main/java/ru/yandex/burgers/model/UserCredentials.java
+++ src/main/java/ru/yandex/burgers/model/UserCredentials.java
@@ -1,29 +1,11 @@
 package ru.yandex.burgers.model;
 
+import lombok.Data;
+
+@Data
 public class UserCredentials {
 
     private final String email;
     private final String password;
-
-    public UserCredentials(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String toString() {
-        return "UserCredentials{" +
-                "email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
 
