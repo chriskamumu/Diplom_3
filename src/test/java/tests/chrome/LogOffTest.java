b72edc6df9bfb6e79c7fa9998224540e54ece05c
Kristina Krylova <kriskamumu@gmail.com>
2022-07-05 21:49:30 +0300

    added UserUtils to generate random test data; added removal of users to tests where they are not expected to created

--- src/test/java/tests/chrome/LogOffTest.java
+++ src/test/java/tests/chrome/LogOffTest.java
@@ -1,47 +1,47 @@
-package chrome;
+package tests.chrome;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.junit.ScreenShooter;
+import io.restassured.response.ValidatableResponse;
 import org.junit.*;
 import ru.yandex.burgers.api.client.AuthClient;
 import ru.yandex.burgers.model.User;
 import ru.yandex.burgers.model.UserCredentials;
 import ru.yandex.burgers.ui.po.ConstructorPagePO;
+import utils.UserUtils;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.apache.hc.core5.http.HttpStatus.SC_OK;
 import static org.apache.http.HttpStatus.SC_ACCEPTED;
 
-public class GoToConstructorTest {
+public class LogOffTest {
 
     private UserCredentials userCredentials;
-    private String accessToken = "";
     private ConstructorPagePO mainPage;
     private AuthClient authClient;
-    String expected = "Соберите бургер";
-    String message = "На странице отсутствует заголовок Конструктора";
 
     @Rule
     public ScreenShooter makeScreenshotOnFailure = ScreenShooter.failedTests().succeededTests();
 
     @Before
     public void setUp() {
-        User user = new User("new_login_kr0307@mail.ru", "password", "name");
+        User user = UserUtils.buildRandom();
         userCredentials = new UserCredentials(user.getEmail(), user.getPassword());
         authClient = new AuthClient();
-        accessToken = authClient.register(user).statusCode(SC_OK).extract().path("accessToken");
+        authClient.register(user).statusCode(SC_OK).extract().path("accessToken");
         mainPage = open(ConstructorPagePO.URL, ConstructorPagePO.class);
     }
 
     @After
     public void tearDown() {
-        if (!accessToken.equals("")) {
+        ValidatableResponse response = authClient.login(userCredentials).assertThat().statusCode(SC_OK);
+        if (response.extract().path("accessToken") != null)
+        {
+            String accessToken = response.extract().path("accessToken");
             authClient.delete(accessToken).assertThat().statusCode(SC_ACCEPTED);
-            accessToken = "";
-        }
-    }
+        }    }
 
     @AfterClass
     public static void tearDownBrowser (){
@@ -49,25 +49,16 @@ public class GoToConstructorTest {
     }
 
     @Test
-    public void testClickConstructorButtonFromAccount(){
-        SelenideElement constructorHeader = mainPage
+    public void testLoggingOff(){
+        SelenideElement signingInHeader = mainPage
                 .clickSignInButton()
                 .fillAllFiledAndClickSignInButton(userCredentials)
                 .clickAccountButtonByAuthorizedUser()
-                .clickConstructorButton()
-                .getConstructorHeader().shouldBe(Condition.visible);
-
-        Assert.assertEquals(message, expected, constructorHeader.getText());
+                .clickLoggingOffButton()
+                .getSigningInHeader().shouldBe(Condition.visible);
+        String expected = "Вход";
+        String message = "Не найден заголовок страницы Авторизации";
+        Assert.assertEquals(message, expected, signingInHeader.getText());
     }
 
-    @Test
-    public void testClickLogoFromAccount(){
-        SelenideElement constructorHeader =  mainPage
-                .clickSignInButton()
-                .fillAllFiledAndClickSignInButton(userCredentials)
-                .clickAccountButtonByAuthorizedUser()
-                .clickLogoButton()
-                .getConstructorHeader().shouldBe(Condition.visible);
-        Assert.assertEquals(message, expected, constructorHeader.getText());
-    }
 }
--- src/test/java/tests/yandex/GoToConstructorTest.java
+++ src/test/java/tests/yandex/GoToConstructorTest.java
@@ -1,25 +1,29 @@
-package yandex;
+package tests.yandex;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.junit.ScreenShooter;
+import io.restassured.response.ValidatableResponse;
 import org.junit.*;
 import ru.yandex.burgers.api.client.AuthClient;
 import ru.yandex.burgers.model.User;
 import ru.yandex.burgers.model.UserCredentials;
 import ru.yandex.burgers.ui.po.ConstructorPagePO;
+import utils.UserUtils;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.apache.hc.core5.http.HttpStatus.SC_OK;
 import static org.apache.http.HttpStatus.SC_ACCEPTED;
 
-public class LogOffTest {
+public class GoToConstructorTest {
 
     private UserCredentials userCredentials;
-    private String accessToken = "";
     private ConstructorPagePO mainPage;
     private AuthClient authClient;
+    String expected = "Соберите бургер";
+    String message = "На странице отсутствует заголовок Конструктора";
 
     @Rule
     public ScreenShooter makeScreenshotOnFailure = ScreenShooter.failedTests().succeededTests();
@@ -27,22 +31,25 @@ public class LogOffTest {
     @BeforeClass
     public static void setupBrowser() {
         System.setProperty("webdriver.chrome.driver", "src/resources/yandexdriver.exe");
+        Configuration.startMaximized = true;
     }
 
     @Before
     public void setUp() {
-        User user = new User("new_login_kr0307@mail.ru", "password", "name");
+        User user = UserUtils.buildRandom();
         userCredentials = new UserCredentials(user.getEmail(), user.getPassword());
         authClient = new AuthClient();
-        accessToken = authClient.register(user).statusCode(SC_OK).extract().path("accessToken");
+        authClient.register(user).statusCode(SC_OK).extract().path("accessToken");
         mainPage = open(ConstructorPagePO.URL, ConstructorPagePO.class);
     }
 
     @After
     public void tearDown() {
-        if (!accessToken.equals("")) {
+        ValidatableResponse response = authClient.login(userCredentials).assertThat().statusCode(SC_OK);
+        if (response.extract().path("accessToken") != null)
+        {
+            String accessToken = response.extract().path("accessToken");
             authClient.delete(accessToken).assertThat().statusCode(SC_ACCEPTED);
-            accessToken = "";
         }
     }
 
@@ -52,16 +59,25 @@ public class LogOffTest {
     }
 
     @Test
-    public void testLoggingOff(){
-        SelenideElement signingInHeader = mainPage
+    public void testClickConstructorButtonFromAccount(){
+        SelenideElement constructorHeader = mainPage
                 .clickSignInButton()
                 .fillAllFiledAndClickSignInButton(userCredentials)
                 .clickAccountButtonByAuthorizedUser()
-                .clickLoggingOffButton()
-                .getSigningInHeader().shouldBe(Condition.visible);
-        String expected = "Вход";
-        String message = "Не найден заголовок страницы Авторизации";
-        Assert.assertEquals(message, expected, signingInHeader.getText());
+                .clickConstructorButton()
+                .getConstructorHeader().shouldBe(Condition.visible);
+
+        Assert.assertEquals(message, expected, constructorHeader.getText());
     }
 
+    @Test
+    public void testClickLogoFromAccount(){
+        SelenideElement constructorHeader =  mainPage
+                .clickSignInButton()
+                .fillAllFiledAndClickSignInButton(userCredentials)
+                .clickAccountButtonByAuthorizedUser()
+                .clickLogoButton()
+                .getConstructorHeader().shouldBe(Condition.visible);
+        Assert.assertEquals(message, expected, constructorHeader.getText());
+    }
 }
